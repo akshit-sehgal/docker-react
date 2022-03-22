c37d3132cdd044980c169435103de765999c88b2
Akshit Sehgal <akshitsehgal95@gmail.com>
2022-03-22 22:49:54 +0530

    Testing Feature branch CI/CD

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Arey beteee, kaise ho yaar?
+          Arey beteee, kaise ho?
         </p>
         <a
           className="App-link"

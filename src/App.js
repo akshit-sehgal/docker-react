6dd34bd4b09483c44341b869c3a7d0428bb06b43
Akshit Sehgal <akshitsehgal@Akshits-MacBook-Pro.local>
2022-03-22 20:27:42 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Arey beteee, kaise ho???
+          Arey beteee, kaise ho yaar?
         </p>
         <a
           className="App-link"
